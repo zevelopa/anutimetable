fce6d6eae2e05da7034ab80d12714e5e4952e7d2
Matthew Chen <zevelopa@gmail.com>
2021-02-15 10:49:33 +1100

    Updated

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '14th of February, 2021';
-var revisionNum     = 160;
+var jsonUpdatedTime = '15th of February, 2021';
+var revisionNum     = 161;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
