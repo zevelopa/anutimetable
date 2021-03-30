9fa5137e75d21beb26bf82a5b98cd833fff41d87
Matthew Chen <zevelopa@gmail.com>
2021-03-31 10:07:02 +1100

    Updated on Wed 31 Mar 2021 10:07:02 AM AEDT

--- js/timetable.js
+++ js/timetable.js
@@ -2,8 +2,8 @@ var rawLessons      = [];
 var timetableData   = {};
 var hasLocalStorage = typeof(Storage) !== 'undefined';
 var recover         = false;
-var jsonUpdatedTime = '28th of March, 2021';
-var revisionNum     = 171;
+var jsonUpdatedTime = '31st of March, 2021';
+var revisionNum     = 172;
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value) {
